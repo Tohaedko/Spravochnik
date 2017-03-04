4e815056ab4fafb24f5e3238ca56937644ed006d
alexander.karpliuk <alexander.karpliuk@synevo.ua>
2017-03-04 20:53:09 +0200

    First

---
+++ Spravochnik/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Spravochnik.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Admin/Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string userName, string password, string returnUrl)
+        {
+            bool result = FormsAuthentication.Authenticate(userName, password);
+            if (result)
+            {
+                FormsAuthentication.SetAuthCookie(userName, false);
+                return Redirect(returnUrl ?? Url.Action("Index", "Home"));
+            }
+            else
+            {
+                // Некорректное имя пользователя или пароль
+                ModelState.AddModelError("", "Incorrect username or password");
+                return View();
+            }
+        }
+    }
+}
---
+++ Spravochnik/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Spravochnik.Filters;
+
+namespace Spravochnik.Controllers
+{
+    [Culture]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ChangeCulture(string lang)
+        {
+            string returnUrl = Request.UrlReferrer.AbsolutePath;
+            // Список культур
+            List<string> cultures = new List<string>() { "ru", "en", "uk" };
+            if (!cultures.Contains(lang))
+            {
+                lang = "ru";
+            }
+            // Сохраняем выбранную культуру в куки
+            HttpCookie cookie = Request.Cookies["lang"];
+            if (cookie != null)
+                cookie.Value = lang;   // если куки уже установлено, то обновляем значение
+            else
+            {
+
+                cookie = new HttpCookie("lang");
+                cookie.HttpOnly = false;
+                cookie.Value = lang;
+                cookie.Expires = DateTime.Now.AddYears(1);
+            }
+            Response.Cookies.Add(cookie);
+            return Redirect(returnUrl);
+        }
+    }
+}
